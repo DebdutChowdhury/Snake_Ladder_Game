642d8618ee23a934937f0dd77b223347cb00cbef
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 16:47:01 +0530

    Added: Generate random number 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,12 @@
 // Snake and Ladder game played with single player at start position 0
+// The Player rolls the die to get a number between 1 to 6.
 import java.util.*;
 class DiceRoll {
 	// constants
 	final int STARTING_POINT = 0;
 	final int END_POINT = 100;
 	static int plr1_pos;
+	static int rand_output;
 	
 	void insert() {
 		// gatting user input
@@ -14,6 +16,11 @@ class DiceRoll {
 		System.out.println("Player name: "+plr1);
 		plr1_pos = STARTING_POINT;
 		System.out.println("Start Player position: "+plr1_pos);
+	}
+	void rolling_dice() {
+		Random rand = new Random();
+		rand_output = rand.nextInt(6) + 1;
+		System.out.println("Dice Roll Output: "+rand_output);
 	}
 }
 
@@ -28,6 +35,7 @@ public class SnakeLadder {
 		
 		DiceRoll dc = new DiceRoll();
 		dc.insert();
+		dc.rolling_dice();
 	}
 
 }
