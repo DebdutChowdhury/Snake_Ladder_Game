361e698bdcabec94f990db6e6e1218f4d05ec007
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 16:23:44 +0530

    Added: Single Player and there initial position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,33 @@
-class SnakeLadder {
+// Snake and Ladder game played with single player at start position 0
+import java.util.*;
+class DiceRoll {
+	// constants
+	final int STARTING_POINT = 0;
+	final int END_POINT = 100;
+	static int plr1_pos;
+	
+	void insert() {
+		// gatting user input
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Player name: ");
+		String plr1 = sc.nextLine();
+		System.out.println("Player name: "+plr1);
+		plr1_pos = STARTING_POINT;
+		System.out.println("Start Player position: "+plr1_pos);
+	}
+}
 
-	public static void main(String[] args) 
-	{
-		// Welcome message here
-		System.out.println("*******************************************");
-		System.out.println("<-:Welcome to the Sneak & Ladder program:->");
-		System.out.println("*******************************************");
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// welcome message here
+		System.out.println("***************************************");
+		System.out.println("<--:Welcome to my Snake Ladder game:-->");
+		System.out.println("***************************************");
+		
+		DiceRoll dc = new DiceRoll();
+		dc.insert();
 	}
 
 }
