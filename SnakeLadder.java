8b5d96f9d89798ee6e4b4063fd630d0fdce860b7
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-23 13:55:54 +0530

    Welcome message here

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+class SnakeLadder {
+
+	public static void main(String[] args) 
+	{
+		// Welcome message here
+		System.out.println("*******************************************");
+		System.out.println("<-:Welcome to the Sneak & Ladder program:->");
+		System.out.println("*******************************************");
+	}
+
+}
